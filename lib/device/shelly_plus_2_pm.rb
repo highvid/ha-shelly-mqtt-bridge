157f105ecb9811a118dc0a9ecb4c2b4dda53e3fa
Manvil George <manvil@gmail.com>
2024-12-20 20:17:06 +1100

    Software update support for Shelly Plus 2PM

--- lib/device/shelly_plus_2_pm.rb
+++ lib/device/shelly_plus_2_pm.rb
@@ -102,29 +102,55 @@ module Device
       model: DEVICE,
       name: -> (entity) { "Output #{entity.unique_id[-1]}"},
       state_topic: -> (entity) { "#{entity.device.publish_topic_prefix}/output/#{entity.name[-1]}" }
-    listener_topics 'status', update_method: :update_info
+    update :sw_version,
+      callback: :call_to_update,
+      command_topic: -> (entity) { "#{entity.device.publish_topic_prefix}/update" },
+      device_class: 'firmware',
+      configuration_url: -> (entity) { "http://#{entity.device.ip_address}" },
+      entity_constructor: ->(device) { { unique_id: "#{device.unique_id}-sw-version", initial_value: nil  } },
+      hw_version: "#{Config::BLIGHVID.capitalize}-#{DEVICE}",
+      identifiers: -> (entity) { [entity.device.unique_id] },
+      json_attributes_topic: -> (entity) { "#{Config::BLIGHVID}/#{entity.unique_id}/attributes" },
+      listener_topics: [ { state: 'events/rpc', state_adapter_method: :sw_version_adapter } ],
+      manufacturer: "#{Config::BLIGHVID}",
+      model: DEVICE,
+      name: 'Firmware',
+      payload_install: 'update',
+      platform: 'update',
+      state_topic: -> (entity) { "#{entity.device.publish_topic_prefix}/firmware" }
+    listener_topics 'status', update_method: :post_status_update
     
     def initialize(**options)
       assign!(options)
-      @announce_topic = generate_topic('command')
-      @announce_payload = 'status_update'
-      @announce_listen_topic = generate_topic('status')
-      @announce_method_adapter = :announce_message_process
-      @post_announce_action = :post_init
+      @announce_topics = {
+        generate_topic('command') => [{
+          listen_topic: generate_topic('announce'),
+          payload: 'announce',
+          process: :receive_announce,
+          post_process: nil
+        }, {
+          listen_topic: generate_topic('status'),
+          payload: 'status_update',
+          process: :receive_status_message,
+          post_process: :post_status_update
+        }]
+      }
       init!(options)
     end
 
-    def announce_message_process(message)
+    def receive_announce(message)
+      $LOGGER.info "Receive announce for #{name}"
+      json_message = JSON.parse(message).deep_symbolize_keys unless message.is_a?(Hash)
+      @current_version = json_message[:ver]
+    end
+
+    def receive_status_message(message)
       json_message = JSON.parse(message).deep_symbolize_keys unless message.is_a?(Hash)
       @ip_address = json_message[:wifi][:sta_ip]
       @device_id = json_message[:sys][:mac]
     end
 
-    def post_init(message)
-      update_info(message)
-    end
-
-    def update_info(message)
+    def post_status_update(message)
       $LOGGER.info "Update info #{name}"
       json_message = JSON.parse(message).deep_symbolize_keys unless message.is_a?(Hash)
       @ip_address = json_message[:wifi][:sta_ip]
@@ -143,6 +169,10 @@ module Device
       @current_1.state = json_message[:'switch:1'][:current]
       @temperature_0.state = json_message[:'switch:0'][:temperature][:tC]
       @temperature_1.state = json_message[:'switch:1'][:temperature][:tC]
+      $LOGGER.info("Setting current version to #{@current_version}")
+      @sw_version.latest_version = json_message[:sys].try(:[], :available_updates).try(:[], :stable).try(:[], :version) || @current_version
+      @sw_version.state = @current_version
+      $LOGGER.info("Setting latest version to #{@sw_version.latest_version}")
     end
 
     def post_state_update(entity_name)
@@ -191,8 +221,38 @@ module Device
       message
     end
 
+    def sw_version_adapter(message)
+      json_message = JSON.parse(message).deep_symbolize_keys unless message.is_a?(Hash)
+      if json_message[:method] == 'NotifyEvent'
+        params = json_message[:params]
+        if params[:events].present? && params[:events].any? { |event| %w[ota_begin ota_progress ota_success].include?(event[:event]) }
+          events = params[:events]
+          events.each do |event|
+            @sw_version.in_progress = %w[ota_begin ota_progress].include?(event[:event])
+            @sw_version.update_percentage = @sw_version.in_progress ? (event[:progress_percent] || 0.0) : nil
+            @current_version = @sw_version.in_progress ? @current_version : @sw_version.latest_version
+          end
+        end
+      end
+      @current_version
+    end
+
+    def call_to_update(message)
+      if message == 'update'
+        $LOGGER.info "Updating #{name} to latest"
+        mqtt_client.publish("shellies/#{unique_id}/command/sys", 'ota_update_to_stable')
+        @sw_version.in_progress = true
+        @sw_version.update_percentage = 0.0
+      end
+      @current_version
+    end
+
     def http_client
       @http_client ||= HttpClient::ShellyPlus2Pm.new(ip_address)
+    end
+
+    def mqtt_client
+      Config.singleton.relay_mqtt
     end
 
     def float_adapter(value)
