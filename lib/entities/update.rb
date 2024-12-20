2e8702c2802b28fe656fc2cea008ea5be6a7fe4b
Manvil George <manvil@gmail.com>
2024-12-20 11:50:03 +1100

    Updating for Shelly Plus 1 PM

--- lib/entities/update.rb
+++ lib/entities/update.rb
@@ -1,13 +1,22 @@
 module Entities
   class Update < Switch
-    attribute :installed_version, in_state: true
-    attribute :latest_version, in_state: true
+    attribute :latest_version, track_update?: true
     attribute :payload_install, default: 'update', in_state: true
-    attribute :in_progress, default: false, in_state: true
-    attribute :update_percentage, default: nil, in_state: true
+    attribute :platform, default: 'update', in_state: true
+    attribute :in_progress, default: false, track_update?: true
+    attribute :update_percentage, default: nil, track_update?: true
     def initialize(unique_id:, initial_value:)
       self.component ||= 'update'
       super
+    end
+
+    def sanitize_value(value)
+      {
+        installed_version: value,
+        latest_version:,
+        in_progress:,
+        update_percentage:
+      }.to_json
     end
   end
 end
