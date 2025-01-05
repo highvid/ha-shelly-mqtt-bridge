b96dde408bbd0abd44551f9f5be04e65bc87d9c7
Manvil George <manvil@gmail.com>
2025-01-06 09:42:17 +1100

    More logging changes

--- lib/device/shelly_1.rb
+++ lib/device/shelly_1.rb
@@ -64,16 +64,16 @@ module Device
     end
 
     def update_info(message)
-      $LOGGER.info "Update info #{name}"
+      $LOGGER.debug "Update info #{name}"
       json_message = JSON.parse(message).deep_symbolize_keys unless message.is_a?(Hash)
       @ip_address = json_message[:wifi_sta][:ip]
       @device_id = json_message[:mac]
       @output.state = json_message[:relays][0][:ison]
       @input.state = json_message[:inputs][0][:input]
-      $LOGGER.info("Setting current version to #{json_message[:update][:old_version]}")
+      $LOGGER.debug("Setting current version to #{json_message[:update][:old_version]}")
       @sw_version.latest_version = json_message[:update][:new_version]
       @sw_version.state = json_message[:update][:old_version]
-      $LOGGER.info("Setting latest version to #{@sw_version.latest_version}")
+      $LOGGER.debug("Setting latest version to #{@sw_version.latest_version}")
     end
 
     def sw_version_adapter(message)
